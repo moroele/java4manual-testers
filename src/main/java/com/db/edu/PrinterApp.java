eb5e0a038b0f4c9287e34877e8b306ff54714797
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-11-14 10:17:53 +0300

    Add demo for javadoc and mvn dependency.

--- src/main/java/com/db/edu/PrinterApp.java
+++ src/main/java/com/db/edu/PrinterApp.java
@@ -3,11 +3,42 @@ package com.db.edu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+iuhgkfjgfkj type
+hlkfjhlkfgjh
+flghkjflgkhj
+flkghjflkhgj
+*/
+
+/**
+ gdfgjh kdfjghdkfgj kjfhgkd fjgh
+
+*/
 public class PrinterApp {
     private static final Logger logger = LoggerFactory.getLogger(PrinterApp.class);
 
+    //kjdfgdkfjgh kdfjghdkgjhdk fjgh
+
+    /**
+     * djhdfkjghdfkg kdjgfhdfkg dkfjgh dgf
+     * @param args
+     * @throws yjkty lktyj ltkj
+     */
     public static void main(String... args) {
         logger.debug("Entering main method with args {}", args);
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        mySuperMethod();
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+
+
+    private static void mySuperMethod() {
+        System.out.println("ffffff");
     }
 }
