a22390d5f459b90eee4c0788f652cdcbaebe6ed2
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-11-21 09:45:02 +0300

    Add Enum implementation.

--- src/main/java/com/db/edu/etl/RecordType.java
+++ src/main/java/com/db/edu/etl/RecordType.java
@@ -3,3 +3,19 @@ package com.db.edu.etl;
 public enum RecordType {
     EIS1_DATA_FILE, EIS2_DATA_FILE, EIS3
 }
+
+class RT2 {
+    public static final RT2 EIS1_DATA_FILE = new RT2("EIS1_DATA_FILE", 1);
+    public static final RT2 EIS2_DATA_FILE = new RT2("EIS2_DATA_FILE", 2);
+    public static final RT2 EIS3 = new RT2("EIS3", 3);
+
+    private String name;
+    private int ordinal;
+
+    private RT2(String name, int ordinal) {
+        this.name = name;
+        this.ordinal = ordinal;
+    }
+}
+
+
