d14a7af2b352942686e86a71182fd0920222e67e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-12-02 10:13:58 +0300

    Add junit demo.

---
+++ src/test/java/com/db/edu/ArrayListTest.java
@@ -0,0 +1,38 @@
+package com.db.edu;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+
+//TODO Refactor to OOP testable design.
+public class ArrayListTest {
+    private ArrayList<String> sut;
+
+    @Before
+    public void setUp() {
+        sut = new ArrayList<>();
+    }
+
+    @Test
+    public void shouldSizeIncrementedWhenElementAdd() {
+        //region Given
+        String dummy = new String();
+        //endregion
+
+        //region When
+        sut.add(dummy);
+        //endregion
+
+        //region Then
+        //if (size != ) throw AssertionError
+        assertEquals(2, sut.size());
+        //endregion
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
--- src/test/java/com/db/edu/PrinterAppTest.java
+++
@@ -1,8 +0,0 @@
-package com.db.edu;
-
-//TODO Refactor to OOP testable design.
-public class PrinterAppTest {
-    public void shouldPrintWhenNormalFlowStringParameter() {
-
-    }
-}
